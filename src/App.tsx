9bcd4b58d9580111edd25d202cd200bed060234d
shahadat52 <shahadathossain.sh255@gmail.com>
2024-01-10 21:28:03 +0600

    Redux store created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-blue-400">
-        Hello world!
-      </h1>
-    </>
+    <div className="h-screen flex justify-center items-center" >
+      <div className="flex justify-center items-center gap-10 ">
+        <button className="bg-green-500 text-white text-xl p-4 rounded-lg">Increment</button>
+        <h1 className="text-5xl font-bold  text-blue-400">
+          0
+        </h1>
+        <button className="bg-red-500 text-xl text-white p-4 rounded-lg">Decrement</button>
+      </div>
+    </div>
   )
 }
 
